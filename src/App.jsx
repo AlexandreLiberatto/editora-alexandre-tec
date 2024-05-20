88885172b62712ac3a7cf7c89fbdb50ea93dfe3b
Alexandre Liberato <alexandreliberatto@gmail.com>
2024-05-20 06:57:15 -0300

    Iniciando o projeto, inserindo imagens e ajustando os arquivos

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <header className='topo'>Topo</header>
+      <main className='principal'>Principal</main>
+      <footer className='rodape'>Rodapé</footer>
+    </div>
+  )
+}
+
+export default App
