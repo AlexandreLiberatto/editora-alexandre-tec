6898d5433991a7a5e40fa180a1b4ca7e2b50202d
Alexandre Liberato <alexandreliberatto@gmail.com>
2024-06-01 15:40:48 -0300

    Corrigindo titulo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Logo = () => (
   <Link to="/">
     <h1 className="logo">
-      <span>Editora MaujorTec</span>
+      <span>Editora Alexandre Tec</span>
     </h1>
   </Link>
 );
